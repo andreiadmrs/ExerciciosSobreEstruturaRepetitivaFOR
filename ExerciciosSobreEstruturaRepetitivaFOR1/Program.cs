36619e996ba4d27569ac6c064e867ad9d732d347
Andreia de Moraes <andreiadmrs@gmail.com>
2020-08-21 21:24:52 -0300

    exercicios do curso da udemy csharp

---
+++ ExerciciosSobreEstruturaRepetitivaFOR1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExerciciosSobreEstruturaRepetitivaFOR1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+            X, se for o caso.*/
+
+            Console.WriteLine("Digite um número:");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= num; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+    }
+}
